afe5fe31cff3e5b5a4b44bd7ed1e19f8cb8565cf
Jing Sheng <teyjingsheng@gmail.com>
2025-10-06 11:00:35 +0800

    Splitting certification into another section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,7 +2,7 @@ import "./Experience.css";
 import BadgeIcon from "../assets/badge.png";
 
 function Experience() {
-  const experiences = [
+  const workExperiences = [
     {
       role: "Data Engineer",
       company: "Hyundai Motor Group Innovation Center Singapore",
@@ -22,11 +22,13 @@ function Experience() {
       role: "Part-time IT Assistant/Administrator",
       company: "NTU Asian School of the Environment",
       period: "2020 – 2023",
-    },
+    }
+  ];
+
+  const certifications = [
     {
       role: "Databricks Certified Data Engineer Associate",
       period: "2025 - 2027",
-      certification: true,
       badge: BadgeIcon,
     }
   ];
@@ -36,26 +38,28 @@ function Experience() {
       <p className="section__text__p1">Explore My</p>
       <h1 className="title">Work Experience</h1>
       <div className="timeline-container">
-        {experiences.map(({ role, company, period, certification, badge }, index) => (
+        {workExperiences.map(({ role, company, period }, index) => (
           <div key={index} className="timeline-item">
             <div className="timeline-dot"></div>
             <div className="timeline-content">
-              {certification ? (
-                // Display certification differently
-                <div className="certification-item">
-                  <h3>{role}</h3>
-                  <span className="period">{period}</span>
-                  <div className="certification-badge">
-                    <img src={badge} alt="Databricks Certification Badge" />
-                  </div>
-                </div>
-              ) : (
-                // Normal work experience
-                <>
-                  <h3>{company} ({role})</h3>
-                  <span className="period">{period}</span>
-                </>
-              )}
+              <h3>{company} ({role})</h3>
+              <span className="period">{period}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <h1 className="title">Certification</h1>
+      <div className="timeline-container">
+        {certifications.map(({ role, period, badge }, index) => (
+          <div key={index} className="timeline-item">
+            <div className="timeline-dot"></div>
+            <div className="timeline-content certification-item">
+              <h3>{role}</h3>
+              <span className="period">{period}</span>
+              <div className="certification-badge">
+                <img src={badge} alt={`${role} Badge`} />
+              </div>
             </div>
           </div>
         ))}
